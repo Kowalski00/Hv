56d6b7bc6d83c48d94af2486f990c0316aa9cc8d
ramonkj <rk0191@hotmail.com>
2021-04-15 10:15:11 -0300

    Problem with FetchType LAZY, method loadUserByUsername now has a tag transaction for a session with db

--- src/main/java/life/heevo/pttp/sb/Application.java
+++ src/main/java/life/heevo/pttp/sb/Application.java
@@ -9,7 +9,7 @@ public class Application {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(new BCryptPasswordEncoder().encode("Pi@8545195"));
+		//System.out.println(new BCryptPasswordEncoder().encode("Pi@8545195"));
 		SpringApplication.run(Application.class, args);
 	}
 }
